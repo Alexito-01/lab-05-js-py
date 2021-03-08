2603671b71758f6d64c65596291f384bb313325d
Alejandro <alejandro1328@gmail.com>
2021-03-08 16:38:45 -0500

    Created the Madlibs in js and py

---
+++ madlibs.py
@@ -0,0 +1,22 @@
+
+series = input("Enter a Series Name: ")
+adjective = input("Enter a adjective: ")
+person = input("Enter the name of the main character from " + series + " : ")
+adjectivetwo = input("Enter a adjective: ")
+noun = input("Enter a noun: ")
+verb = input("Enter a verb: ")
+number = input("Enter a number: ")
+adjectivefour = input("Enter a adjective: ")
+nountwo = input("Enter a noun: ")
+nounthree = input("Enter a noun: ")
+nounfour = input("Enter a noun: ")
+console.log(series + " is the " + str(adjective) + " Series ever." + 
+" The main character is " + str(person) + ", the plot was" + str(adjectivetwo) + "," + 
+" and the " + str(noun) + " was very fun to look at." + 
+" I would " + str(verb) + " this series if I had the chance." + 
+" If only " + series + " was real." + 
+" This deserves " + str(number) + " star(s) for being the " + str(adjectivefour) + " series in history." + 
+" I can't wait for the next season." + 
+" Hopefully they add " + str(nountwo) + " to it." + 
+" That would blow my " + str(nounthree) + "." + 
+" My " + str(nounfour) + " is shivering just thinking about it.")
